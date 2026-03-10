e54d2a22d9c4a0b36dccb8c4c37e50a51f3164c4
Justin <justin.david.michel@gmail.com>
2026-03-10 08:09:09 -0500

    Use FluentTheme.Palettes feature to simplify colors

---
+++ src/DevMentalMd.App/Controls/ControlGalleryWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace DevMentalMd.App.Controls;
+
+public partial class ControlGalleryWindow : Window {
+    public ControlGalleryWindow() {
+        InitializeComponent();
+    }
+}
